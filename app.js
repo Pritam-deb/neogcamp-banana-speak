91424f947764381c47882773a033043ebde79a3f
Pritam <pritamdebnath297@gmail.com>
2021-04-07 23:56:10 +0530

    feat : added a button and textarea  that takes input

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Translated you thing.")
+console.log("Translated you thing.");
+// var userName = prompt("Enter your name");
+
+// alert("you are in right page "+userName);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+console.log(btnTranslate);
+
+function clickEvent() {
+    console.log("Clicked!");
+    console.log("value", textInput.value);
+
+}
+
+outputDiv.innerHTML("yo")
+
+btnTranslate.addEventListener("click", clickEvent);
