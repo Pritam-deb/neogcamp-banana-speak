d1170a50ec287e7a6ba5e478d68c7664c6b6d94b
Pritam <pritamdebnath297@gmail.com>
2021-04-08 20:55:29 +0530

    test : Done testing the translator
    The english to banana translator is working. API of funtranslation is used successfully.

--- app.js
+++ app.js
@@ -8,11 +8,25 @@ var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 console.log(btnTranslate);
 
+serverURL = "https://api.funtranslations.com/translate/minion.json";
+function Link(text){
+    return serverURL+"?text="+text;
+}
+
+
+
+
 function clickEvent() {
     // console.log("Clicked!");
     // console.log("value", textInput.value);
     // console.log("value",outputDiv.innerText);
-    outputDiv.innerText = "done with translation: "+textInput.value;
+    fetch(Link(textInput.value))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    //outputDiv.innerText = "done with translation: "+textInput.value;
 }
 
 
